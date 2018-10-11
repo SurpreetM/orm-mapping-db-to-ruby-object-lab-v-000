e72f878d9f0d8179dc6a7178a1ef5d1886cfde6c
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-11 16:46:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,8 +67,17 @@ class Student
   end
 
   def self.first_student_in_grade_10
-
-  end 
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
 
   def save
