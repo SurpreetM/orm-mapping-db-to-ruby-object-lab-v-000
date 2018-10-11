384fed99ca55fb0f2af7e3db6617911b34927738
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-11 16:32:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
       DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
       end
-    end 
+    end
 
   def self.find_by_name(name)
     sql = <<-SQL
@@ -27,6 +27,18 @@ class Student
       LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end.first
   end
