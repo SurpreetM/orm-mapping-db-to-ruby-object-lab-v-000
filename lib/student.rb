6ed9b5eacc302084545c154b9312828803c6393b
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-11 16:39:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
       FROM students
       WHERE grade = ?
     SQL
-    DB[:conn].execute(sql, <12).map do |row|
+    DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end
   end
@@ -48,7 +48,7 @@ class Student
       FROM students
       WHERE grade = ?
     SQL
-    DB[:conn].execute(sql, 9).map do |row|
+    DB[:conn].execute(sql, <12).map do |row|
       self.new_from_db(row)
     end
   end
